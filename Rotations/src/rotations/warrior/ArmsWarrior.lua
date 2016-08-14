c6625ae5820c1d63628b6aa0a4d7ba7428202bd0
Phine <phine-eredar@users.noreply.github.com>
2016-08-14 12:27:24 -0700

    arms and prot warrior

--- Rotations/src/rotations/warrior/ArmsWarrior.lua
+++ Rotations/src/rotations/warrior/ArmsWarrior.lua
@@ -1,10 +1,57 @@
 function PhineRotations:ArmsWarrior()
 
-  local talents = {}
+  local talents = {
+    ["Fervor of Battle"] = {
+      tier = 3,
+      column = 1
+    }
+  }
 
-  local single = {}
+  local single = { {
+    conditions = { {
+      type = "debuff",
+      name = "Colossus Smash",
+      active = true
+    } },
+    ability = "Execute"
+  },{
+    ability = "Rend"
+  }, {
+    ability = "Colossus Smash"
+  }, {
+    ability = "Overpower"
+  }, {
+    ability = "Mortal Strike"
+  }, {
+    conditions = { {
+      type = "talent",
+      name = "Fervor of Battle",
+      active = true
+    } },
+    ability = "Whirlwind"
+  }, {
+    conditions = { {
+      type = "talent",
+      name = "Fervor of Battle",
+      active = false
+    } },
+    ability = "Slam"
+  } }
 
-  local multi = {}
+  local multi = { {
+    conditions = { {
+      type = "buff",
+      name = "Cleave",
+      active = false
+    } },
+    ability = "Cleave"
+  }, {
+    ability = "Mortal Strike"
+  }, {
+    ability = "Whirlwind"
+  }, {
+    ability = "Colossus Smash"
+  } }
 
   return {
     talents = function() return talents end,
--- Rotations/src/rotations/warrior/ProtectionWarrior.lua
+++ Rotations/src/rotations/warrior/ProtectionWarrior.lua
@@ -1,10 +1,59 @@
 function PhineRotations:ProtectionWarrior()
 
-  local talents = {}
+  local talents = {
+    Vengeance = {
+      tier = 6,
+      column = 1
+    }
+  }
 
-  local single = {}
+  local single = { {
+    conditions = { {
+      type = "power",
+      powerType = 1,
+      operator = ">=",
+      value = 60
+    } },
+    ability = "Ignore Pain"
+  }, {
+    conditions = { {
+      type = "buff",
+      name = "Shield Block",
+      active = false
+    } },
+    ability = "Shield Block"
+  }, {
+    ability = "Shield Slam"
+  }, {
+    ability = "Revenge"
+  }, {
+    ability = "Devastate"
+  } }
 
-  local multi = {}
+  local multi = { {
+    conditions = { {
+      type = "power",
+      powerType = 1,
+      operator = ">=",
+      value = 60
+    } },
+    ability = "Ignore Pain"
+  }, {
+    conditions = { {
+      type = "buff",
+      name = "Shield Block",
+      active = false
+    } },
+    ability = "Shield Block"
+  }, {
+    ability = "Revenge"
+  }, {
+    ability = "Thunder Clap"
+  }, {
+    ability = "Shield Slam"
+  }, {
+    ability = "Devastate"
+  } }
 
   return {
     talents = function() return talents end,
