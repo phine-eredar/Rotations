677109d65df5d98b45d1e0f00603e962deded184
Phine <phine-eredar@users.noreply.github.com>
2016-08-14 12:50:42 -0700

    elemental shaman

--- Rotations/src/rotations/shaman/ElementalShaman.lua
+++ Rotations/src/rotations/shaman/ElementalShaman.lua
@@ -2,9 +2,66 @@ function PhineRotations:ElementalShaman()
 
   local talents = {}
 
-  local single = {}
+  local single = { {
+    conditions = { {
+      type = "debuff",
+      name = "Flame Shock",
+      active = false
+    } },
+    ability = "Flame Shock"
+    }, {
+    conditions = { {
+      type = "power",
+      powerType = 11,
+      operator = ">=",
+      value = 100
+    } },
+    ability = "Earth Shock"
+  }, {
+    ability = "Lava Burst"
+  }, {
+    conditions = { {
+      type = "power",
+      powerType = 11,
+      operator = ">",
+      value = 90
+    } },
+    ability = "Earth Shock"
+  }, {
+    ability = "Lightning Bolt"
+  } }
 
-  local multi = {}
+  local multi = { {
+    conditions = { {
+      type = "debuff",
+      name = "Flame Shock",
+      active = false
+    } },
+    ability = "Flame Shock"
+    }, {
+    conditions = { {
+      type = "power",
+      powerType = 11,
+      operator = ">=",
+      value = 100
+    } },
+    ability = "Earth Shock"
+  }, {
+    ability = "Lava Burst"
+  }, {
+    ability = "Earthquake Totem"
+  }, {
+    conditions = { {
+      type = "power",
+      powerType = 11,
+      operator = ">",
+      value = 90
+    } },
+    ability = "Earth Shock"
+  }, {
+    ability = "Chain Lightning"
+  } }
+
 
   return {
     talents = function() return talents end,
