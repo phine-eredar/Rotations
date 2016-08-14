ceb8b9d8f40fe171b95e0ea2f5ddab28cc490ece
Phine <phine-eredar@users.noreply.github.com>
2016-08-14 11:54:41 -0700

    new spec placeholders

---
+++ Rotations/src/rotations/druid/BalanceDruid.lua
@@ -0,0 +1,15 @@
+function PhineRotations:BalanceDruid()
+
+  local talents = {}
+
+  local single = {}
+
+  local multi = {}
+
+  return {
+    talents = function() return talents end,
+    single = function() return single end,
+    multi = function() return multi end
+  }
+
+end
