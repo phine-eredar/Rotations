6dbf2b6ac2a8b6bc3bafc93a35f6b60ef36b90ef
Phine <phine-eredar@users.noreply.github.com>
2017-06-11 16:03:33 -0700

    shadow priest

--- Rotations/src/rotations/priest/ShadowPriest.lua
+++ Rotations/src/rotations/priest/ShadowPriest.lua
@@ -1,8 +1,20 @@
 function PhineRotations:ShadowPriest()
 
-  local talents = {}
+  local talents = {
+    ["Misery"] = {
+      tier = 6,
+      column = 2
+    }
+  }
 
   local single = { {
+    conditions = { {
+      type = "buff",
+      name = "Voidform",
+      active = true
+    } },
+    ability = "Void Torrent"
+  }, {
     conditions = { {
       type = "buff",
       name = "Voidform",
@@ -27,9 +39,16 @@ function PhineRotations:ShadowPriest()
     ability = "Mind Blast"
   }, {
     conditions = { {
-      type = "debuff",
-      name = "Shadow Word: Pain",
-      active = false
+      type = "and",
+      children = { {
+        type = "talent",
+        name = "Misery",
+        active = false
+      }, {
+        type = "debuff",
+        name = "Shadow Word: Pain",
+        active = false
+      } }
     } },
     ability = "Shadow Word: Pain"
   }, {
@@ -43,7 +62,60 @@ function PhineRotations:ShadowPriest()
     ability = "Mind Flay"
   } }
 
-  local multi = {}
+  local multi = { {
+    conditions = { {
+      type = "buff",
+      name = "Voidform",
+      active = true
+    } },
+    ability = "Void Bolt"
+  },  {
+    conditions = { {
+      type = "buff",
+      name = "Voidform",
+      active = true
+    } },
+    ability = "Shadow Crash"
+  }, {
+    conditions = { {
+      type = "buff",
+      name = "Voidform",
+      active = true
+    } },
+    ability = "Shadow Word: Death"
+  }, {
+    conditions = { {
+      type = "buff",
+      name = "Voidform",
+      active = false
+    } },
+    ability = "Void Eruption"
+  }, {
+    ability = "Mind Blast"
+  }, {
+    conditions = { {
+      type = "and",
+      children = { {
+        type = "talent",
+        name = "Misery",
+        active = false
+      }, {
+        type = "debuff",
+        name = "Shadow Word: Pain",
+        active = false
+      } }
+    } },
+    ability = "Shadow Word: Pain"
+  }, {
+    conditions = { {
+      type = "debuff",
+      name = "Vampiric Touch",
+      active = false
+    } },
+    ability = "Vampiric Touch"
+  }, {
+    ability = "Mind Flay"
+  } }
 
   return {
     talents = function() return talents end,
