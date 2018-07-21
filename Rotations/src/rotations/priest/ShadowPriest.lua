248572d112429946f2927bad3bd5fbf0f8498b9d
Phine <phine-eredar@users.noreply.github.com>
2018-07-21 16:39:54 -0700

    shadow priest 8.0

--- Rotations/src/rotations/priest/ShadowPriest.lua
+++ Rotations/src/rotations/priest/ShadowPriest.lua
@@ -1,77 +1,26 @@
+local Buffed = PhineRotations.Buffed
+local Debuffed = PhineRotations.Debuffed
+local Not = PhineRotations.Not
+local Or = PhineRotations.Or
+local Rotation = PhineRotations.Rotation
+
 function PhineRotations:ShadowPriest()
-
   local talents = {}
-  
-  local single = { {
-    conditions = { {
-      type = "debuff",
-      name = "Vampiric Touch",
-      active = false
-    } },
-    ability = "Vampiric Touch"
-  }, {
-    conditions = { {
-      type = "debuff",
-      name = "Shadow Word: Pain",
-      active = false
-    } },
-    ability = "Shadow Word: Pain"
-  }, {
-    conditions = { {
-      type = "buff",
-      name = "Voidform",
-      active = true
-    } },
-    ability = "Void Bolt"
-  }, {
-    conditions = { {
-      type = "buff",
-      name = "Voidform",
-      active = true
-    } },
-    ability = "Shadow Word: Death"
-  }, {
-    ability = "Mind Blast"
-  }, {
-    conditions = { {
-      type = "buff",
-      name = "Voidform",
-      active = true
-    } },
-    ability = "Void Torrent"
-  }, {
-    conditions = { {
-      type = "buff",
-      name = "Voidform",
-      active = true
-    } },
-    ability = "Mindbender"
-  }, {
-    conditions = { {
-      type = "buff",
-      name = "Voidform",
-      active = true
-    } },
-    ability = "Shadowfiend"
-  }, {
-    conditions = { {
-      type = "buff",
-      name = "Voidform",
-      active = false
-    } },
-    ability = "Void Eruption"
-  }, {
-    ability = "Mind Blast"
-  }, {
-    ability = "Mind Flay"
-  } }
 
-  local multi = {}
+  local single = Rotation()
+  single.use("Void Eruption").when(Not(Buffed("Voidform")))
+  single.use("Vampiric Touch").whenAll(Not(Buffed("Voidform")), Or(Not(Debuffed("Shadow Word: Pain")), Not(Debuffed("Vampiric Touch"))))
+  single.use("Void Bolt").when(Buffed("Voidform"))
+  single.use("Mind Blast")
+  single.use("Mindbender").when(Buffed("Voidform"))
+  single.use("Shadowfiend").when(Buffed("Voidform"))
+  single.use("Mind Flay")
+
+  local multi = Rotation()
 
   return {
     talents = function() return talents end,
-    single = function() return single end,
-    multi = function() return multi end
+    single = function() return single.get() end,
+    multi = function() return multi.get() end
   }
-
 end
