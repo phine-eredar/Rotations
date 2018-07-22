387239685cea0c43ec7b2ed022a0c5a1095b0344
Phine <phine-eredar@users.noreply.github.com>
2018-07-22 09:04:21 -0700

    protection paladin 8.0

--- Rotations/src/rotations/paladin/ProtectionPaladin.lua
+++ Rotations/src/rotations/paladin/ProtectionPaladin.lua
@@ -1,15 +1,27 @@
-function PhineRotations:ProtectionPaladin()
+local Buffed = PhineRotations.Buffed
+local Not = PhineRotations.Not
+local Rotation = PhineRotations.Rotation
 
+function PhineRotations:ProtectionPaladin()
   local talents = {}
 
-  local single = {}
+  local single = Rotation()
+  single.use("Consecration").when(Not(Buffed("Consecration")))
+  single.use("Judgment")
+  single.use("Avenger's Shield")
+  single.use("Hammer of the Righteous")
+  single.use("Consecration")
 
-  local multi = {}
+  local multi = Rotation()
+  multi.use("Consecration").when(Not(Buffed("Consecration")))
+  multi.use("Judgment")
+  multi.use("Avenger's Shield")
+  multi.use("Hammer of the Righteous")
+  multi.use("Consecration")
 
   return {
     talents = function() return talents end,
-    single = function() return single end,
-    multi = function() return multi end
+    single = function() return single.get() end,
+    multi = function() return multi.get() end
   }
-
 end
