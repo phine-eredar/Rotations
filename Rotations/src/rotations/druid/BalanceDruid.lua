ae15b568c8151f517aa34589a5876ea9357f4c38
Phine <phine-eredar@users.noreply.github.com>
2018-07-21 17:49:11 -0700

    balance druid 8.0

--- Rotations/src/rotations/druid/BalanceDruid.lua
+++ Rotations/src/rotations/druid/BalanceDruid.lua
@@ -1,15 +1,30 @@
-function PhineRotations:BalanceDruid()
+local Buffed = PhineRotations.Buffed
+local Debuffed = PhineRotations.Debuffed
+local Not = PhineRotations.Not
+local Rotation = PhineRotations.Rotation
 
+function PhineRotations:BalanceDruid()
   local talents = {}
 
-  local single = {}
+  local single = Rotation()
+  single.use("Moonfire").when(Not(Debuffed("Moonfire")))
+  single.use("Sunfire").when(Not(Debuffed("Sunfire")))
+  single.use("Solar Wrath").when(Buffed("Solar Empowerment"))
+  single.use("Lunar Strike").when(Buffed("Lunar Empowerment"))
+  single.use("Starsurge")
+  single.use("Solar Wrath")
 
-  local multi = {}
+  local multi = Rotation()
+  multi.use("Starfall")
+  multi.use("Sunfire").when(Not(Debuffed("Sunfire")))
+  multi.use("Moonfire").when(Not(Debuffed("Moonfire")))
+  multi.use("Lunar Strike").when(Buffed("Lunar Empowerment"))
+  multi.use("Solar Wrath").when(Buffed("Solar Empowerment"))
+  multi.use("Lunar Strike")
 
   return {
     talents = function() return talents end,
-    single = function() return single end,
-    multi = function() return multi end
+    single = function() return single.get() end,
+    multi = function() return multi.get() end
   }
-
 end
