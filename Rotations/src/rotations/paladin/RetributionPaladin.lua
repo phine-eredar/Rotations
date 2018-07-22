3beb83b2f31150ec7a9fb2709d49051434b40146
Phine <phine-eredar@users.noreply.github.com>
2018-07-22 08:56:02 -0700

    retribution paladin 8.0

--- Rotations/src/rotations/paladin/RetributionPaladin.lua
+++ Rotations/src/rotations/paladin/RetributionPaladin.lua
@@ -1,125 +1,28 @@
-function PhineRotations:RetributionPaladin()
+local Power = PhineRotations.Power
+local Rotation = PhineRotations.Rotation
 
+function PhineRotations:RetributionPaladin()
   local talents = {}
 
-  local single = { {
-    conditions = { {
-      type = "debuff",
-      name = "Judgment",
-      active = true
-    } },
-    ability = "Templar's Verdict"
-  }, {
-    conditions = { {
-      type = "debuff",
-      name = "Judgment",
-      active = true
-    } },
-    ability = "Execution Sentence"
-  }, {
-    conditions = { {
-      type = "and",
-      children = { {
-        type = "power",
-        powerType = 9,
-        operator = "<=",
-        value = 4
-      }, {
-        type = "charges",
-        operator = "==",
-        value = 2
-      } }
-    } },
-    ability = "Crusader Strike"
-  }, {
-    conditions = { {
-      type = "power",
-      powerType = 9,
-      operator = "<=",
-      value = 3
-    } },
-    ability = "Divine Hammer"
-  }, {
-    conditions = { {
-      type = "power",
-      powerType = 9,
-      operator = "<=",
-      value = 3
-    } },
-    ability = "Blade of Justice"
-  }, {
-    conditions = { {
-      type = "power",
-      powerType = 9,
-      operator = "<=",
-      value = 4
-    } },
-    ability = "Crusader Strike"
-  }, {
-    ability = "Judgment"
-  } }
+  local single = Rotation()
+  single.use("Avenging Wrath")
+  single.use("Templar's Verdict").when(Power(">=", 5, 9))
+  single.use("Wake of Ashes")
+  single.use("Blade of Justice")
+  single.use("Judgment")
+  single.use("Crusader Strike")
 
-  local multi = { {
-    conditions = { {
-      type = "debuff",
-      name = "Judgment",
-      active = true
-    } },
-    ability = "Divine Storm"
-  }, {
-    conditions = { {
-      type = "debuff",
-      name = "Judgment",
-      active = true
-    } },
-    ability = "Execution Sentence"
-  }, {
-    conditions = { {
-      type = "and",
-      children = { {
-        type = "power",
-        powerType = 9,
-        operator = "<=",
-        value = 4
-      }, {
-        type = "charges",
-        operator = "==",
-        value = 2
-      } }
-    } },
-    ability = "Crusader Strike"
-  }, {
-    conditions = { {
-      type = "power",
-      powerType = 9,
-      operator = "<=",
-      value = 3
-    } },
-    ability = "Divine Hammer"
-  }, {
-    conditions = { {
-      type = "power",
-      powerType = 9,
-      operator = "<=",
-      value = 3
-    } },
-    ability = "Blade of Justice"
-  }, {
-    conditions = { {
-      type = "power",
-      powerType = 9,
-      operator = "<=",
-      value = 4
-    } },
-    ability = "Crusader Strike"
-  }, {
-    ability = "Judgment"
-  } }
+  local multi = Rotation()
+  multi.use("Avenging Wrath")
+  multi.use("Divine Storm").when(Power(">=", 5, 9))
+  multi.use("Wake of Ashes")
+  multi.use("Blade of Justice")
+  multi.use("Judgment")
+  multi.use("Crusader Strike")
 
   return {
     talents = function() return talents end,
-    single = function() return single end,
-    multi = function() return multi end
+    single = function() return single.get() end,
+    multi = function() return multi.get() end
   }
-
 end
