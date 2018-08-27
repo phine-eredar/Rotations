dec5de57b4aab89cc27a4199d719153c7555c7a3
Phine <phine-eredar@users.noreply.github.com>
2018-08-27 10:16:34 -0700

    update ele shaman

--- Rotations/src/rotations/shaman/ElementalShaman.lua
+++ Rotations/src/rotations/shaman/ElementalShaman.lua
@@ -1,22 +1,20 @@
-local Buffed = PhineRotations.Buffed
 local Debuffed = PhineRotations.Debuffed
 local Not = PhineRotations.Not
+local Power = PhineRotations.Power
 local Rotation = PhineRotations.Rotation
 
 function PhineRotations:ElementalShaman()
   local talents = {}
 
   local single = Rotation()
-  single.use("Totem Mastery").whenAny(Not(Buffed("Resonance Totem")), Not(Buffed("Storm Totem")), Not(Buffed("Ember Totem")), Not(Buffed("Tailwind Totem")))
   single.use("Flame Shock").when(Not(Debuffed("Flame Shock")))
   single.use("Fire Elemental")
-  single.use("Earth Shock")
+  single.use("Earth Shock").when(Power(">=", 60, 11))
   single.use("Lava Burst")
   single.use("Lightning Bolt")
 
   local multi = Rotation()
   multi.use("Fire Elemental")
-  multi.use("Stormkeeper")
   multi.use("Earthquake")
   multi.use("Chain Lightning")
 
