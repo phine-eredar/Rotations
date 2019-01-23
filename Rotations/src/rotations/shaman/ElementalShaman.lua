453fdd2a6d8e93b32dafd58b06b0f4c6050332b0
Phine <phine-eredar@users.noreply.github.com>
2019-01-23 13:58:28 -0800

    elemental shaman easy mode

--- Rotations/src/rotations/shaman/ElementalShaman.lua
+++ Rotations/src/rotations/shaman/ElementalShaman.lua
@@ -1,33 +1,24 @@
-local Buffed = PhineRotations.Buffed
-local Charges = PhineRotations.Charges
 local Debuffed = PhineRotations.Debuffed
-local Name = PhineRotations.Name
+local Totem = PhineRotations.Totem
 local Not = PhineRotations.Not
 local Power = PhineRotations.Power
 local Rotation = PhineRotations.Rotation
-local Totem = PhineRotations.Totem
 
 function PhineRotations:ElementalShaman()
   local talents = {}
 
   local single = Rotation()
-  single.use("Totem Mastery").when(Not(Buffed("Resonance Totem")))
-  single.use("Flame Shock").whenAll(Not(Debuffed("Flame Shock", 6)), Charges("<=", 13, "Wind Gust"))
-  single.use("Storm Elemental")
-  single.use("Earth Elemental").when(Name("pet", nil))
-  single.use("Earth Shock").when(Power(">=", 90, 11))
-  single.use("Stormkeeper")
-  single.use("Lightning Bolt").when(Buffed("Stormkeeper"))
+  single.use("Flame Shock").when(Not(Debuffed("Flame Shock", 6)))
+  single.use("Earth Elemental").when(Not(Totem()))
+  single.use("Fire Elemental").when(Not(Totem()))
+  single.use("Earth Shock").when(Power(">=", 60, 11))
   single.use("Lava Burst").when(Debuffed("Flame Shock"))
-  single.use("Totem Mastery").when(Totem(9))
-  single.use("Earth Shock").whenAll(Power(">=", 60, 11), Not(Buffed("Lava Surge")))
   single.use("Lightning Bolt")
 
   local multi = Rotation()
-  multi.use("Totem Mastery").when(Not(Buffed("Resonance Totem")))
-  multi.use("Flame Shock").when(Not(Debuffed("Flame Shock", 6)))
-  multi.use("Earthquake")
-  multi.use("Stormkeeper")
+  multi.use("Fire Elemental").when(Not(Totem()))
+  multi.use("Earth Elemental").when(Not(Totem()))
+  multi.use("Earthquake").when(Power(">=", 60, 11))
   multi.use("Chain Lightning")
 
   return {
