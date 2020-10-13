4a6635fb72e2907ad79abf4ff8f14b9e8ed2b6f5
Phine <phine-eredar@users.noreply.github.com>
2020-10-13 14:46:16 -0700

    shadowlands enh shaman

--- Rotations/src/rotations/shaman/EnhancementShaman.lua
+++ Rotations/src/rotations/shaman/EnhancementShaman.lua
@@ -1,35 +1,43 @@
 local Buffed = PhineRotations.Buffed
+local Charges = PhineRotations.Charges
+local Debuffed = PhineRotations.Debuffed
 local Not = PhineRotations.Not
-local Power = PhineRotations.Power
 local Rotation = PhineRotations.Rotation
 
 function PhineRotations:EnhancementShaman()
-  local talents = {}
+    local talents = {}
 
-  local single = Rotation()
-  single.use("Lightning Shield").when(Not(Buffed("Lightning Shield")))
-  single.use("Flametongue").when(Not(Buffed("Flametongue")))
-  single.use("Stormstrike")
-  single.use("Flametongue")
-  single.use("Sundering")
-  single.use("Rockbiter").when(Power("<", 70))
-  single.use("Lava Lash").when(Power(">", 40))
-  single.use("Rockbiter")
+    local single = Rotation()
+    --single.use("Windfury Weapon").when(Not(Buffed("Windfury Weapon")))
+    --single.use("Flametongue Weapon").when(Not(Buffed("Flametongue Weapon")))
+    single.use("Lightning Shield").when(Not(Buffed("Lightning Shield")))
+    single.use("Flame Shock").when(Not(Debuffed("Flame Shock", 6)))
+    single.use("Stormstrike")
+    single.use("Lava Lash")
+    single.use("Frost Shock")
+    single.use("Crash Lightning")
+    single.use("Lightning Bolt").when(Charges(">=", 5, "Maelstrom Weapon"))
 
-  local multi = Rotation()
-  multi.use("Lightning Shield").when(Not(Buffed("Lightning Shield")))
-  multi.use("Crash Lightning")
-  multi.use("Flametongue").when(Not(Buffed("Flametongue")))
-  multi.use("Stormstrike")
-  multi.use("Flametongue")
-  multi.use("Sundering")
-  multi.use("Rockbiter").when(Power("<", 70))
-  multi.use("Lava Lash").when(Power(">", 40))
-  multi.use("Rockbiter")
+    local multi = Rotation()
+    --multi.use("Windfury Weapon").when(Not(Buffed("Windfury Weapon")))
+    --multi.use("Flametongue Weapon").when(Not(Buffed("Flametongue Weapon")))
+    multi.use("Lightning Shield").when(Not(Buffed("Lightning Shield")))
+    multi.use("Crash Lightning")
+    multi.use("Flame Shock").when(Not(Debuffed("Flame Shock", 6)))
+    multi.use("Stormstrike")
+    multi.use("Lava Lash")
+    multi.use("Frost Shock")
+    multi.use("Chain Lightning").when(Charges(">=", 5, "Maelstrom Weapon"))
 
-  return {
-    talents = function() return talents end,
-    single = function() return single.get() end,
-    multi = function() return multi.get() end
-  }
+    return {
+        talents = function()
+            return talents
+        end,
+        single = function()
+            return single.get()
+        end,
+        multi = function()
+            return multi.get()
+        end
+    }
 end
