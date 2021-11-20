d2548493a400ff089dd72ba50bafce3d0ee7a7e4
Phine <phine-eredar@users.noreply.github.com>
2021-11-20 14:32:03 -0800

    feral druid 9.1.5

--- Rotations/src/rotations/druid/FeralDruid.lua
+++ Rotations/src/rotations/druid/FeralDruid.lua
@@ -1,29 +1,30 @@
-local Buffed = PhineRotations.Buffed
 local Combo = PhineRotations.Combo
 local Debuffed = PhineRotations.Debuffed
 local Not = PhineRotations.Not
 local Power = PhineRotations.Power
+local POWER = PhineRotations.POWER
 local Rotation = PhineRotations.Rotation
 
 function PhineRotations:FeralDruid()
   local talents = {}
 
   local single = Rotation()
+  single.use("Tiger's Fury").when(Power("<", 30, POWER.ENERGY))
   single.use("Berserk")
-  single.use("Tiger's Fury").whenAny(Power("<", 40, 3), Buffed("Berserk"))
+  single.use("Ferocious Bite").whenAll(Combo(">=", 5), Debuffed("Rip"))
   single.use("Rip").whenAll(Combo(">=", 5), Not(Debuffed("Rip")))
   single.use("Rake").when(Not(Debuffed("Rake")))
-  single.use("Ferocious Bite").whenAll(Combo(">=", 5), Debuffed("Rip"))
+  single.use("Brutal Slash")
   single.use("Shred")
 
   local multi = Rotation()
+  multi.use("Tiger's Fury").when(Power("<", 30, POWER.ENERGY))
   multi.use("Berserk")
-  multi.use("Tiger's Fury").whenAny(Power("<", 40, 3), Buffed("Berserk"))
+  multi.use("Ferocious Bite").whenAll(Combo(">=", 5), Debuffed("Rip"))
   multi.use("Rip").whenAll(Combo(">=", 5), Not(Debuffed("Rip")))
   multi.use("Rake").when(Not(Debuffed("Rake")))
-  multi.use("Thrash")
-  multi.use("Ferocious Bite").whenAll(Combo(">=", 5), Debuffed("Rip"))
-  multi.use("Swipe")
+  multi.use("Thrash").when(Not(Debuffed("Thrash")))
+  multi.use("Brutal Slash")
   multi.use("Shred")
 
   return {
